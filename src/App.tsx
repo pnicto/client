ec24246f0247fe113889abb52ad5b78008ecf237
pnicto <thehulk489@gmail.com>
2022-08-15 19:27:25 +0530

    Add mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import MainNavbar from "./components/MainNavbar";
+import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: { main: "#3385ff" },
+  },
+  shape: {
+    borderRadius: 10,
+  },
+});
 
 function App() {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <MainNavbar />
+      </>
+    </ThemeProvider>
+  );
 }
 
 export default App;
