b34ea78ddd419fd85ae411acdfe0e7e56c221b85
pnicto <thehulk489@gmail.com>
2022-08-16 08:53:40 +0530

    Finish basic footer

--- src/components/MainFooter.tsx
+++ src/components/MainFooter.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { Button } from "@mui/material";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+interface Tasksboard {
+  id: number;
+  boardTitle: string;
+}
 
 const MainFooter = () => {
-  return <div>MainFooter</div>;
+  const [tasksboards, setTasksboards] = useState<Tasksboard[] | never[]>([]);
+
+  useEffect(() => {
+    const fetchAllTasksboards = async () => {
+      const response = await axios.get("http://localhost:5000/api/tasksboards");
+      if (response.status === 200) {
+        console.log(response.data);
+        setTasksboards(response.data);
+      }
+    };
+    fetchAllTasksboards();
+  }, []);
+
+  return (
+    <div id="main-footer">
+      {tasksboards.map((tasksboard, index) => {
+        return (
+          <Button key={tasksboard.id} variant="contained" color="secondary">
+            {tasksboard.boardTitle}
+          </Button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default MainFooter;
