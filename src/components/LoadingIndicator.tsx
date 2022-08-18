2bdf8de2514d78149586c68c0ce7c093b933e426
pnicto <thehulk489@gmail.com>
2022-08-18 21:31:34 +0530

    Add loading indicator

---
+++ src/components/LoadingIndicator.tsx
@@ -0,0 +1,11 @@
+import { CircularProgress } from "@mui/material";
+
+const LoadingIndicator = () => {
+  return (
+    <div id="loading-indicator">
+      <CircularProgress color="primary" />
+    </div>
+  );
+};
+
+export default LoadingIndicator;
