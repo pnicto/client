9b75fb4184203f7e7d5bb4fbc763e7115bc722ec
pnicto <thehulk489@gmail.com>
2022-08-18 21:31:17 +0530

    Finish dynamic board title

--- src/components/MainNavbar.tsx
+++ src/components/MainNavbar.tsx
@@ -1,12 +1,18 @@
-import React from "react";
 import { Button, IconButton } from "@mui/material/";
 import { MoreHoriz, MoreVert } from "@mui/icons-material";
+import { useGlobalContext } from "../context/appContext";
 
 const MainNavbar = () => {
+  const { globalState } = useGlobalContext();
+  const { activeTasksboardId, tasksboards } = globalState;
+  const activeTasksboard = tasksboards.find(
+    (tasksboard) => tasksboard.id === activeTasksboardId
+  );
+
   return (
     <nav id="main-navbar">
       <div id="board-title">
-        <h3>Board title</h3>
+        <h3>{activeTasksboard?.boardTitle}</h3>
         <IconButton aria-label="more board actions">
           <MoreVert />
         </IconButton>
