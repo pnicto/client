3ce1f4b1663f2e5f52fe6bf74ad41225b21a16c0
pnicto <thehulk489@gmail.com>
2022-08-19 14:51:06 +0530

    Use misc dialog component

--- src/components/Taskcard.tsx
+++ src/components/Taskcard.tsx
@@ -1,17 +1,10 @@
 import { AddCircle, MoreVert } from "@mui/icons-material";
-import {
-  Button,
-  Card,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  IconButton,
-  TextField,
-} from "@mui/material";
+import { Card, IconButton, List } from "@mui/material";
 import axios from "axios";
-import { useRef, useState } from "react";
-import { TaskcardInterface } from "../interfaces/interfaces";
+import { useEffect, useRef, useState } from "react";
+import { TaskcardInterface, TaskitemInterface } from "../interfaces/interfaces";
+import AddDialog from "./misc/AddDialog";
+import Taskitem from "./Taskitem";
 
 type Props = {
   taskcard: TaskcardInterface;
@@ -19,7 +12,23 @@ type Props = {
 const Taskcard = ({ taskcard }: Props) => {
   const [open, setOpen] = useState(false);
   const taskRef = useRef<HTMLInputElement>();
+  const [tasks, setTasks] = useState<TaskitemInterface[]>([]);
 
+  const fetchAllTasks = async () => {
+    const url = `${process.env.REACT_APP_BASE_URL}/tasks/${taskcard.id}`;
+    const getResponse = await axios.get(url);
+    const responseData: TaskitemInterface[] = getResponse.data;
+    if (getResponse.status === 200) {
+      setTasks(responseData);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllTasks();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // Dialog controls
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -30,14 +39,15 @@ const Taskcard = ({ taskcard }: Props) => {
 
   const handleAddTask = async () => {
     const taskToBeAdded = taskRef.current?.value;
-    const postBody = {
-      taskTitle: taskToBeAdded,
-    };
     if (taskToBeAdded) {
-      setOpen(false);
+      const postBody = {
+        taskTitle: taskToBeAdded,
+      };
       const url = `${process.env.REACT_APP_BASE_URL}/tasks/${taskcard.id}`;
       const postResponse = await axios.post(url, postBody);
-      console.log(postResponse.data);
+      const newTask = postResponse.data;
+      setTasks([...tasks, newTask]);
+      setOpen(false);
     } else {
       setOpen(true);
     }
@@ -51,41 +61,27 @@ const Taskcard = ({ taskcard }: Props) => {
           <IconButton onClick={handleClickOpen}>
             <AddCircle color="primary" />
           </IconButton>
-          <AddTaskDialog />
+          <AddDialog
+            dialogLabel="Add a task"
+            dialogTitle="Task"
+            fieldRef={taskRef}
+            handleClose={handleClose}
+            handleSubmit={handleAddTask}
+            open={open}
+          />
           <IconButton>
             <MoreVert />
           </IconButton>
           {/* TODO:Menu for more options. */}
         </div>
       </div>
+      <List>
+        {tasks.map((task) => {
+          return <Taskitem key={task.id}></Taskitem>;
+        })}
+      </List>
     </Card>
   );
-
-  function AddTaskDialog() {
-    return (
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Add a task</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Add a task"
-            type="text"
-            fullWidth
-            variant="standard"
-            inputRef={taskRef}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button variant="contained" onClick={handleAddTask}>
-            Add
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  }
 };
 
 export default Taskcard;
