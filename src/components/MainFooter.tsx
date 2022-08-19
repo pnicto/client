20599df91f9cf3b4235cd67657cef215255ab569
pnicto <thehulk489@gmail.com>
2022-08-19 14:50:32 +0530

    Finish footer

--- src/components/MainFooter.tsx
+++ src/components/MainFooter.tsx
@@ -1,12 +1,45 @@
 import { Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 import { useGlobalContext } from "../context/appContext";
+import { useRef, useState } from "react";
+import AddDialog from "./misc/AddDialog";
+import axios from "axios";
 
 const MainFooter = () => {
+  const [open, setOpen] = useState(false);
+  const taskboardRef = useRef<HTMLInputElement>();
   const { globalState, setGlobalState } = useGlobalContext();
   const { tasksboards, activeTasksboardId } = globalState;
 
   const changeActiveTasksboard = (tasksboardId: number) => {
     setGlobalState({ ...globalState, activeTasksboardId: tasksboardId });
+  };
+
+  // Dialog controls
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleAddTask = async () => {
+    const newBoardTitle = taskboardRef.current?.value;
+    if (newBoardTitle) {
+      const postBody = {
+        taskboardTitle: newBoardTitle,
+      };
+      const url = `${process.env.REACT_APP_BASE_URL}/taskboards`;
+      const postResponse = await axios.post(url, postBody);
+      const newTaskboard = postResponse.data;
+      setGlobalState({
+        ...globalState,
+        tasksboards: [...tasksboards, newTaskboard],
+      });
+      setOpen(false);
+    }
+    console.log(newBoardTitle);
   };
 
   return (
@@ -38,6 +71,17 @@ const MainFooter = () => {
           );
         }
       })}
+      <Button onClick={handleClickOpen}>
+        <AddIcon />
+      </Button>
+      <AddDialog
+        dialogLabel="Create a tasksboard"
+        dialogTitle="Name"
+        fieldRef={taskboardRef}
+        handleClose={handleClose}
+        handleSubmit={handleAddTask}
+        open={open}
+      />
     </div>
   );
 };
