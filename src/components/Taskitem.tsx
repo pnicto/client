59f173c2d7ae0d69b57ede2701752038cdcfbff7
pnicto <thehulk489@gmail.com>
2022-08-20 18:06:49 +0530

    Finish task completed status css and db status

--- src/components/Taskitem.tsx
+++ src/components/Taskitem.tsx
@@ -7,13 +7,30 @@ import {
   ListItemSecondaryAction,
   ListItemText,
 } from "@mui/material";
+import axios from "axios";
+import React, { useState } from "react";
 import { TaskitemInterface } from "../interfaces/interfaces";
 
 const Taskitem = ({ id, completed, description, title }: TaskitemInterface) => {
+  const [isComplete, setIsComplete] = useState(completed);
+
+  const changeCompletionStatus = async (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    const url = `${process.env.REACT_APP_BASE_URL}/tasks/${id}`;
+    await axios.patch(url, {
+      completed: !isComplete,
+    });
+    setIsComplete(!isComplete);
+  };
+
   return (
-    <ListItem disablePadding={true}>
+    <ListItem
+      disablePadding={true}
+      className={`task-item ${isComplete && "complete"}`}
+    >
       <ListItemButton disableRipple={true} disableGutters={true}>
-        <Checkbox onClick={() => console.log("CLicked checkbox")} />
+        <Checkbox onClick={changeCompletionStatus} checked={isComplete} />
         <ListItemText>{title}</ListItemText>
         <ListItemSecondaryAction>
           <IconButton onClick={() => console.log("Pressed edit")}>
