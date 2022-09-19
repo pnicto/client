237521d95dd7bdbc970b61cf492c46916288e4b1
pnicto <thehulk489@gmail.com>
2022-09-19 16:40:21 +0530

    Finish alert snackbar component

---
+++ src/components/misc/AlertSnackbar.tsx
@@ -0,0 +1,43 @@
+import { Snackbar, AlertProps } from "@mui/material";
+import MuiAlert, { AlertColor } from "@mui/material/Alert";
+import React from "react";
+import { useGlobalContext } from "../../context/appContext";
+
+const AlertSnackbar = () => {
+  const { globalDispatch, globalState } = useGlobalContext();
+  const { isOpen, message, severity } = globalState.snackbarState;
+
+  return (
+    <Snackbar
+      open={isOpen}
+      autoHideDuration={4000}
+      onClose={(_: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === "clickaway") return;
+        globalDispatch({
+          type: "close snackbar",
+        });
+      }}
+    >
+      <Alert
+        onClose={(_: React.SyntheticEvent | Event, reason?: string) => {
+          if (reason === "clickaway") return;
+          globalDispatch({
+            type: "close snackbar",
+          });
+        }}
+        severity={severity as AlertColor}
+        sx={{ width: "100%" }}
+      >
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default AlertSnackbar;
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props,
+  ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
