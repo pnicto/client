0e9ad4e57e33e59fae4b35f725e40d06065ddab8
pnicto <thehulk489@gmail.com>
2022-09-19 17:45:33 +0530

    Add functionality redirect to login if no access

--- src/components/misc/LoadingIndicator.tsx
+++ src/components/misc/LoadingIndicator.tsx
@@ -1,26 +1,30 @@
 import { CircularProgress } from "@mui/material";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useGlobalContext } from "../../context/appContext";
 
 const LoadingIndicator = () => {
-  const { globalState, globalDispatch } = useGlobalContext();
-  const { user } = globalState;
-  const token = sessionStorage.getItem("token");
+  const { globalDispatch } = useGlobalContext();
   const navigate = useNavigate();
+  const token = sessionStorage.getItem("token");
+  const user = sessionStorage.getItem("user");
 
-  if (!user || !token) {
-    globalDispatch({
-      type: "update snackbar",
-      payload: {
-        isOpen: true,
-        message: "Invalid action. Please login/register to continue.",
-        severity: "error",
-      },
-    });
-    navigate("/form", {
-      state: "login",
-    });
-  }
+  useEffect(() => {
+    if (!token && !user) {
+      globalDispatch({
+        type: "update snackbar",
+        payload: {
+          isOpen: true,
+          message: "Invalid action. Please login/register to continue.",
+          severity: "error",
+        },
+      });
+      navigate("/form", {
+        state: "login",
+      });
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [token, user]);
 
   return (
     <div id="loading-indicator">
