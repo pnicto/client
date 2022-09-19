56f57a263f479dacc4add5bbf11d6fe4eada3b80
pnicto <thehulk489@gmail.com>
2022-09-19 16:40:59 +0530

    If no token or user navigate to login/register

--- src/components/misc/LoadingIndicator.tsx
+++ src/components/misc/LoadingIndicator.tsx
@@ -1,6 +1,27 @@
 import { CircularProgress } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useGlobalContext } from "../../context/appContext";
 
 const LoadingIndicator = () => {
+  const { globalState, globalDispatch } = useGlobalContext();
+  const { user } = globalState;
+  const token = sessionStorage.getItem("token");
+  const navigate = useNavigate();
+
+  if (!user || !token) {
+    globalDispatch({
+      type: "update snackbar",
+      payload: {
+        isOpen: true,
+        message: "Invalid action. Please login/register to continue.",
+        severity: "error",
+      },
+    });
+    navigate("/form", {
+      state: "login",
+    });
+  }
+
   return (
     <div id="loading-indicator">
       <CircularProgress color="primary" />
