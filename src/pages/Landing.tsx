64a95bb75a364cf0a56bd7ae8468123b887ebd09
pnicto <thehulk489@gmail.com>
2022-09-19 16:42:11 +0530

    Add alert snackbar

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,27 +2,31 @@ import { ReactComponent as LandingTasksSvg } from "../assets/landingTasks.svg";
 import TaskRoundedIcon from "@mui/icons-material/TaskRounded";
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import AlertSnackbar from "../components/misc/AlertSnackbar";
 
 const Landing = () => {
   return (
-    <div id="landing-page">
-      <div id="tasks-heading">
-        <h1>TASKS</h1>
-        <TaskRoundedIcon className="tasks-icon" />
+    <>
+      <div id="landing-page">
+        <div id="tasks-heading">
+          <h1>TASKS</h1>
+          <TaskRoundedIcon className="tasks-icon" />
+        </div>
+        <LandingTasksSvg id="tasks-svg" />
+        <Button variant="contained">
+          <Link to="/form" className="btn-link" state={"register"}>
+            Register
+          </Link>
+        </Button>
+        <p>
+          Already a user?{" "}
+          <Link to={"/form"} state={"login"}>
+            Login
+          </Link>
+        </p>
       </div>
-      <LandingTasksSvg id="tasks-svg" />
-      <Button variant="contained">
-        <Link to="/form" className="btn-link" state={"register"}>
-          Register
-        </Link>
-      </Button>
-      <p>
-        Already a user?{" "}
-        <Link to={"/form"} state={"login"}>
-          Login
-        </Link>
-      </p>
-    </div>
+      <AlertSnackbar />
+    </>
   );
 };
 
