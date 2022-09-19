d6ed68b55a11f6966397bbbbcf2a7c8fa6c6ba17
pnicto <thehulk489@gmail.com>
2022-09-19 16:44:48 +0530

    Finish googleOauth, add snackbar

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,12 +1,14 @@
-import { Snackbar, TextField, AlertProps } from "@mui/material";
+import { TextField } from "@mui/material";
 import TaskRoundedIcon from "@mui/icons-material/TaskRounded";
 import LoadingButton from "@mui/lab/LoadingButton";
-import {  useRef, useState } from "react";
-import axios from "axios";
-import MuiAlert, { AlertColor } from "@mui/material/Alert";
+import { useEffect, useRef, useState } from "react";
+import axios, { AxiosResponse } from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 import { useGlobalContext } from "../context/appContext";
-import React from "react";
+import AlertSnackbar from "../components/misc/AlertSnackbar";
+
+const token = sessionStorage.getItem("token");
 
 const Register = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -15,16 +17,38 @@ const Register = () => {
   const passwordRef = useRef<HTMLInputElement>();
   const usernameRef = useRef<HTMLInputElement>();
   const { globalDispatch } = useGlobalContext();
+
   const navigate = useNavigate();
-  const [snackbarState, setSnackbarState] = useState<{
-    isOpen: boolean;
-    message: string;
-    severity: "danger" | "success" | "info";
-  }>({
-    isOpen: false,
-    message: "",
-    severity: "info",
-  });
+
+  useEffect(() => {
+    if (token) {
+      navigate("/app");
+      globalDispatch({
+        type: "update snackbar",
+        payload: {
+          isOpen: true,
+          message: "Login successful",
+          severity: "success",
+        },
+      });
+    }
+    google.accounts.id.initialize({
+      client_id: process.env.REACT_APP_CLIENT_ID as string,
+      callback: handleCredentialResponse,
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("btn-group") as HTMLElement,
+      {
+        theme: "filled_blue",
+        type: "standard",
+        text: pageMode === "register" ? "signup_with" : "signin_with",
+        shape: "rectangular",
+      }
+    );
+    google.accounts.id.prompt();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pageMode]);
 
   const handleFormSubmit = async () => {
     setIsLoading(true);
@@ -32,7 +56,16 @@ const Register = () => {
     const password = passwordRef.current?.value;
     const username = usernameRef.current?.value;
 
-    if (!email && !password) throw Error("Empty fields");
+    if (!email && !password) {
+      globalDispatch({
+        type: "update snackbar",
+        payload: {
+          isOpen: true,
+          message: "Please provide the required details",
+          severity: "error",
+        },
+      });
+    }
     let postBody: {
       email: string | undefined;
       password?: string;
@@ -44,25 +77,38 @@ const Register = () => {
     const url = `${process.env.REACT_APP_BASE_URL}/user/${pageMode}`;
     if (pageMode === "register" && username) {
       postBody = { ...postBody, username };
+    } else {
+      globalDispatch({
+        type: "update snackbar",
+        payload: {
+          isOpen: true,
+          message: "Please provide the required details",
+          severity: "error",
+        },
+      });
     }
-    const postResponse = await axios.post(url, postBody, {
-      withCredentials: true,
-    });
+    const postResponse = await axios.post(url, postBody);
 
     const { user } = postResponse.data;
 
     if (pageMode === "login" && postResponse.status === 200 && user) {
       setIsLoading(false);
       navigate("/app");
-      setSnackbarState({
-        ...snackbarState,
-        isOpen: true,
-        message: "Login successful",
-        severity: "success",
-      });
       globalDispatch({
         type: "login user",
         payload: user as { email: string; id: number; username: string },
+      });
+      globalDispatch({
+        type: "set session token",
+        payload: postResponse.data.accessToken,
+      });
+      globalDispatch({
+        type: "update snackbar",
+        payload: {
+          isOpen: true,
+          message: "Login successful",
+          severity: "success",
+        },
       });
     }
 
@@ -71,89 +117,140 @@ const Register = () => {
       navigate("/form", {
         state: "login",
       });
-      setSnackbarState({
-        ...snackbarState,
-        isOpen: true,
-        severity: "success",
-        message: "Registration successful. Please login to continue",
+      globalDispatch({
+        type: "update snackbar",
+        payload: {
+          isOpen: true,
+          severity: "success",
+          message: "Registration successful. Please login to continue",
+        },
       });
     }
   };
 
+  const handleCredentialResponse = async (
+    response: google.accounts.id.CredentialResponse
+  ) => {
+    const { email, email_verified, name, iss } = jwt_decode(
+      response.credential
+    ) as {
+      email: string;
+      email_verified: string;
+      name: string;
+      iss: string;
+    };
+
+    if (email_verified) {
+      const url = `${process.env.REACT_APP_BASE_URL}/user/${pageMode}`;
+      const postBody = {
+        email,
+        username: name,
+        iss,
+      };
+
+      let postResponse: AxiosResponse<any, any>;
+      switch (pageMode) {
+        case "login":
+          postResponse = await axios.post(url, postBody);
+          if (postResponse.status === 200) {
+            globalDispatch({
+              type: "login user",
+              payload: postResponse.data.user as {
+                email: string;
+                id: number;
+                username: string;
+              },
+            });
+            navigate("/app");
+            globalDispatch({
+              type: "set session token",
+              payload: postResponse.data.accessToken,
+            });
+            globalDispatch({
+              type: "update snackbar",
+              payload: {
+                isOpen: true,
+                message: "Login successful",
+                severity: "success",
+              },
+            });
+          }
+          break;
+        case "register":
+          postResponse = await axios.post(url, postBody);
+          if (postResponse.status === 201) {
+            navigate("/form", {
+              state: "login",
+            });
+            globalDispatch({
+              type: "update snackbar",
+              payload: {
+                isOpen: true,
+                severity: "success",
+                message: "Registration successful. Please login to continue",
+              },
+            });
+          }
+
+          break;
+      }
+    }
+  };
+
   return (
-    <div id="register">
-      <form
-        id="user-form"
-        onSubmit={(event) => {
-          event.preventDefault();
-          handleFormSubmit();
-        }}
-      >
-        <div id="tasks-heading">
-          <h1>TASKS</h1>
-          <TaskRoundedIcon className="tasks-icon" />
-        </div>
-        <TextField
-          inputRef={emailRef}
-          type="email"
-          variant="outlined"
-          label="Email"
-          fullWidth
-          size="small"
-          className="login-field"
-          autoFocus
-        />
-        <TextField
-          inputRef={passwordRef}
-          variant="outlined"
-          type="password"
-          label="Password"
-          fullWidth
-          size="small"
-          className="login-field"
-        />
-        {/* Render username field if the page mode is register */}
-        {pageMode === "register" && (
+    <>
+      <div id="register">
+        <form
+          id="user-form"
+          onSubmit={(event) => {
+            event.preventDefault();
+            handleFormSubmit();
+          }}
+        >
+          <div id="tasks-heading">
+            <h1>TASKS</h1>
+            <TaskRoundedIcon className="tasks-icon" />
+          </div>
           <TextField
-            inputRef={usernameRef}
+            inputRef={emailRef}
+            type="email"
             variant="outlined"
-            label="Username"
+            label="Email"
+            fullWidth
+            size="small"
+            className="login-field"
+            autoFocus
+          />
+          <TextField
+            inputRef={passwordRef}
+            variant="outlined"
+            type="password"
+            label="Password"
             fullWidth
             size="small"
             className="login-field"
           />
-        )}
-        <LoadingButton type="submit" variant="contained" loading={isLoading}>
-          {pageMode ?? "Login"}
-        </LoadingButton>
-        <p>Or</p>
-      </form>
-      <div id="btn-group"></div>
-      <Snackbar
-        open={snackbarState.isOpen}
-        autoHideDuration={4000}
-        onClose={() => {
-          setSnackbarState({ ...snackbarState, isOpen: false });
-        }}
-      >
-        <Alert
-          onClose={() => {
-            setSnackbarState({ ...snackbarState, isOpen: false });
-          }}
-          severity={snackbarState.severity as AlertColor}
-          sx={{ width: "100%" }}
-        >
-          {snackbarState.message}
-        </Alert>
-      </Snackbar>
-    </div>
+          {/* Render username field if the page mode is register */}
+          {pageMode === "register" && (
+            <TextField
+              inputRef={usernameRef}
+              variant="outlined"
+              label="Username"
+              fullWidth
+              size="small"
+              className="login-field"
+            />
+          )}
+          <LoadingButton type="submit" variant="contained" loading={isLoading}>
+            {pageMode ?? "Login"}
+          </LoadingButton>
+          <p>Or</p>
+        </form>
+        <div id="btn-group"></div>
+      </div>
+      <AlertSnackbar />
+    </>
   );
 };
 
-const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
-  props,
-  ref
-) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-});
 export default Register;
