941a6abc33e371a4676cc661670195621adf9783
pnicto <thehulk489@gmail.com>
2022-09-24 11:38:58 +0530

    Add GoogleOAuthProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AppProvider } from "./context/appContext";
 import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,7 +12,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AppProvider>
-        <App />
+        <GoogleOAuthProvider
+          clientId={process.env.REACT_APP_CLIENT_ID as string}
+        >
+          <App />
+        </GoogleOAuthProvider>
       </AppProvider>
     </BrowserRouter>
   </React.StrictMode>
