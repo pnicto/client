16adf2380b9c1a4b92223bf7fafed3bc933705e7
pnicto <thehulk489@gmail.com>
2022-09-25 22:14:31 +0530

    Finish basic date time picker component

---
+++ src/components/misc/BasicDateTimePicker.tsx
@@ -0,0 +1,25 @@
+import { Dayjs } from "dayjs";
+import TextField from "@mui/material/TextField";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateTimePicker } from "@mui/x-date-pickers";
+type Props = {
+  date: Dayjs | null;
+  handleDateChange: (newDate: Dayjs | null) => void;
+  label: string;
+};
+const BasicDateTimePicker = ({ date, handleDateChange, label }: Props) => {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateTimePicker
+        label={label}
+        disablePast
+        value={date}
+        onChange={handleDateChange}
+        renderInput={(params: any) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+  );
+};
+
+export default BasicDateTimePicker;
